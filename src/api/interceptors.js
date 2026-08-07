0604977c734d9cb133e8e629e58d13010c3a0f61
Adsuumus <53020395+Adsuumus@users.noreply.github.com>
2026-08-07 22:54:42 +0700

    Обновил интерцептор, чтобы редирект был только из профиля

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -1,7 +1,10 @@
 import { clearAuth } from "@/utils/auth";
 
 export const handleAuthError = (error) => {
-  if (error.response?.status === 401) {
+  if (
+    error.response?.status === 401 &&
+    window.location.pathname.startsWith("/profile")
+  ) {
     clearAuth();
     window.location.href = `${import.meta.env.BASE_URL}login/`;
   }
